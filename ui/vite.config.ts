3a5b69f819e4023ce3e78dff5e1357517f157dc2
Jeremy Dohin <arkantik.pro@gmail.com>
2025-09-27 23:13:45 +0200

    refactor: 💡 Update of configs

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,9 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig, mergeConfig } from 'vite';
+import { defineConfig as defineVitestConfig } from 'vitest/config';
 
-export default defineConfig({
-	plugins: [sveltekit()],
+const viteConfig = defineConfig({
+	plugins: [sveltekit()]
+});
+
+const vitestConfig = defineVitestConfig({
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
 });
+
+export default mergeConfig(viteConfig, vitestConfig);
