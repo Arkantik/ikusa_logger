16d2f661e30f8b3b9e279df08c6c37140fcdafb8
Jeremy Dohin <arkantik.pro@gmail.com>
2025-10-01 19:32:11 +0200

    refactor: 💡 Update of config files

--- build.bat
+++ build.bat
@@ -2,19 +2,18 @@
 cd logger
 CALL install.bat
 
-:: Copy everything form logger/dist/logger to dist/ikusa-logger/logger/
+:: Copy everything from logger/dist/logger to dist/ikusa-logger/logger/
 cd .. 
 xcopy logger\dist\logger dist\ikusa-logger\logger\ /E /Y
 
-
-
-:: Install Dependencines the Frontend
-cd ui 
+:: Install Dependencies for the Frontend (React)
+cd client 
 CALL npm i
 
+:: Install Neutralino CLI globally
 CALL npm i -g @neutralinojs/neu@11.3.1
 
-:: Compile the program
+:: Compile the React program
 cd .. 
 CALL neu update
 CALL neu build
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,29 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: "inject-neutralino",
+      transformIndexHtml(html) {
+        return html.replace(
+          '<body class="dark">',
+          '<body class="dark">\n    <script src="/__neutralino_globals.js"></script>'
+        );
+      },
+    },
+  ],
   resolve: {
     alias: {
       "@": "/src",
     },
   },
+  build: {
+    outDir: "build",
+    emptyOutDir: true,
+  },
+  base: "./",
 });
