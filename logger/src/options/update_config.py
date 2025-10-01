8b3226874b814ac8994d5760dac305f4ca0af4cd
Jeremy Dohin <arkantik.pro@gmail.com>
2025-10-01 21:59:11 +0200

    refactor: 💡 Update of retrieve url to update

--- logger/src/options/update_config.py
+++ logger/src/options/update_config.py
@@ -3,7 +3,7 @@ from .. import config
 from . import status_check
 
 def update_config():
-    urllib.request.urlretrieve("https://raw.githubusercontent.com/sch-28/combat_logger/main/config.ini", "config.ini")
+    urllib.request.urlretrieve("https://github.com/Arkantik/ikusa_logger/blob/main/config.ini", "config.ini")
     config.init()
     if(status_check.is_outdated()):
         print("The config is still outdated. Please update it manually.", flush=True)
