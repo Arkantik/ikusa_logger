07a05b1319eb3b66033911f058c1475bb07a4a9b
Jeremy Dohin <arkantik.pro@gmail.com>
2025-10-01 19:35:14 +0200

    refactor: 💡 Add of types for proper build

--- client/src/hooks/useClickOutside.ts
+++ client/src/hooks/useClickOutside.ts
@@ -1,4 +1,5 @@
-import { useEffect, RefObject } from "react";
+import { useEffect } from "react";
+import type { RefObject } from "react";
 
 export function useClickOutside(ref: RefObject<HTMLElement>, handler: (event: MouseEvent | TouchEvent) => void) {
   useEffect(() => {
